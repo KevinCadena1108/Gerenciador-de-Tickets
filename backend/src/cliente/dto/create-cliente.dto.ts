516b7a0ea7ae852909fb744b9f84d00aa09b7e4c
luca <lukzbe@hotmail.com>
2024-09-17 20:35:34 -0300

    UPDATE fix date validation and add error messages to create cliente dto

--- backend/src/cliente/dto/create-cliente.dto.ts
+++ backend/src/cliente/dto/create-cliente.dto.ts
@@ -1,54 +1,45 @@
 import { Exclude } from 'class-transformer';
-import {
-  IsDate,
-  IsEmail,
-  IsInt,
-  IsNotEmpty,
-  IsString,
-  Length,
-  MaxLength,
-  Min,
-  MinLength,
-} from 'class-validator';
+import { IsDateString, IsEmail, IsInt, IsNotEmpty } from 'class-validator';
+import { IsString, Length, MaxLength, Min, MinLength } from 'class-validator';
 
 export class CreateClienteDto {
-  @IsString()
-  @IsNotEmpty()
-  @Length(11, 11)
+  @IsString({ message: 'O CPF deve ser uma string' })
+  @IsNotEmpty({ message: 'O CPF não pode ser vazio' })
+  @Length(11, 11, { message: 'O CPF deve ter 11 caracteres' })
   cpf: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(3)
+  @IsString({ message: 'O nome deve ser uma string' })
+  @IsNotEmpty({ message: 'O nome não pode ser vazio' })
+  @MinLength(3, { message: 'O nome deve ter no mínimo 3 caracteres' })
   nome: string;
 
-  @IsEmail()
-  @IsNotEmpty()
-  @MinLength(3)
-  @MaxLength(320)
+  @IsEmail({}, { message: 'O email deve ser um email válido' })
+  @IsNotEmpty({ message: 'O email não pode ser vazio' })
+  @MinLength(3, { message: 'O email deve ter no mínimo 3 caracteres' })
+  @MaxLength(320, { message: 'O email deve ter no máximo 320 caracteres' })
   email: string;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(8)
-  @MaxLength(20)
+  @IsString({ message: 'O telefone deve ser uma string' })
+  @IsNotEmpty({ message: 'O telefone não pode ser vazio' })
+  @MinLength(8, { message: 'O telefone deve ter no mínimo 8 caracteres' })
+  @MaxLength(20, { message: 'O telefone deve ter no máximo 20 caracteres' })
   telefone: string;
 
-  @IsDate()
+  @IsDateString({}, { message: 'O nascimento deve ser uma data válida' })
   nascimento: Date;
 
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(8)
+  @IsString({ message: 'A senha deve ser uma string' })
+  @IsNotEmpty({ message: 'A senha não pode ser vazia' })
+  @MinLength(8, { message: 'A senha deve ter no mínimo 8 caracteres' })
   @Exclude()
   senha: string;
 
-  @IsInt()
-  @Min(0)
+  @IsInt({ message: 'O id da categoria deve ser um número inteiro' })
+  @Min(0, { message: 'O id da categoria deve ser um número positivo' })
   idCategoria: number;
 
-  @IsString()
-  @IsNotEmpty()
-  @Length(10, 10)
+  @IsString({ message: 'O número de matrícula deve ser uma string' })
+  @IsNotEmpty({ message: 'O número de matrícula não pode ser vazio' })
+  @Length(10, 10, { message: 'O número de matrícula deve ter 10 caracteres' })
   numeroMatricula: string;
 }
