29b58f71022e3d99d8e32d2ab6a9a512389cd1c6
luca <lukzbe@hotmail.com>
2024-09-17 21:13:56 -0300

    ADD create cliente route to backend

--- backend/src/cliente/cliente.controller.ts
+++ backend/src/cliente/cliente.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { CreateClienteDto } from './dto/create-cliente.dto';
+import { ClienteService } from './cliente.service';
 
 @Controller('cliente')
-export class ClienteController {}
+export class ClienteController {
+  constructor(private readonly clienteService: ClienteService) {}
+
+  @Post()
+  async create(@Body() createClienteDto: CreateClienteDto) {
+    return await this.clienteService.createCliente(createClienteDto);
+  }
+}
--- backend/src/cliente/cliente.module.ts
+++ backend/src/cliente/cliente.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClienteController } from './cliente.controller';
 import { ClienteService } from './cliente.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ClienteController],
   providers: [ClienteService],
+  imports: [PrismaModule, AuthModule]
 })
 export class ClienteModule {}
