5a0edac5f3064820be768a7ca10abd35ec98e287
Lucas <lukzbe@hotmail.com>
2024-10-08 10:11:55 -0300

    ADD auth guard

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,30 +1,11 @@
-import { Controller, UnauthorizedException } from '@nestjs/common';
-import { ClienteRepository } from 'src/cliente/cliente.repository';
+import { Controller } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
-  constructor(
-    private readonly clienteRepository: ClienteRepository,
-    private readonly authService: AuthService,
-    private readonly jwtService: JwtService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   async signIn(cpf: string, password: string) {
-    const cliente = await this.clienteRepository.findOne({ cpf });
-    const badRequest = new UnauthorizedException('CPF ou senha inválidos');
-    if (!cliente) {
-      throw badRequest;
-    }
-
-    const hashedPassword = cliente.senha;
-    const isValidPass = this.authService.verifyHash(password, hashedPassword);
-    if (!isValidPass) {
-      throw badRequest;
-    }
-
-    const payload = { sub: cliente.id, isAdministrador: cliente.isAdministrador };
-    return { access_token: await this.jwtService.signAsync(payload) };
+    return await this.authService.signIn(cpf, password);
   }
 }
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { randomBytes, scryptSync, createHash } from 'crypto';
+import { ClienteRepository } from 'src/cliente/cliente.repository';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly clienteRepository: ClienteRepository,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async signIn(cpf: string, password: string) {
+    const cliente = await this.clienteRepository.findOne({ cpf });
+    const badRequest = new UnauthorizedException('CPF ou senha inválidos');
+    if (!cliente) {
+      throw badRequest;
+    }
+
+    const hashedPassword = cliente.senha;
+    const isValidPass = this.verifyHash(password, hashedPassword);
+    if (!isValidPass) {
+      throw badRequest;
+    }
+
+    const payload = { sub: cliente.id, isAdministrador: cliente.isAdministrador };
+    return { access_token: await this.jwtService.signAsync(payload) };
+  }
+
   hashPassword(password: string, passwordSalt?: string): string {
     const salt = passwordSalt ?? randomBytes(32).toString('hex');
     const pepper = createHash('sha256').update(password).digest('hex');
