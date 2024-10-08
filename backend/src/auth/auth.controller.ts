0e832d38e1b0e779bbaf67ef9f0cb41f9529ee24
Lucas <lukzbe@hotmail.com>
2024-10-08 09:43:35 -0300

    ADD jwt to auth route

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, UnauthorizedException } from '@nestjs/common';
 import { ClienteRepository } from 'src/cliente/cliente.repository';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auth')
 export class AuthController {
   constructor(
     private readonly clienteRepository: ClienteRepository,
     private readonly authService: AuthService,
+    private readonly jwtService: JwtService,
   ) {}
 
   async signIn(cpf: string, password: string) {
@@ -22,9 +24,7 @@ export class AuthController {
       throw badRequest;
     }
 
-    return {
-      id: cliente.id,
-      isAdministrador: cliente.isAdministrador,
-    };
+    const payload = { sub: cliente.id, isAdministrador: cliente.isAdministrador };
+    return { access_token: await this.jwtService.signAsync(payload) };
   }
 }
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { ClienteModule } from 'src/cliente/cliente.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [AuthService],
   exports: [AuthService],
   controllers: [AuthController],
-  imports: [ClienteModule],
+  imports: [
+    ClienteModule,
+    JwtModule.registerAsync({
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1d' },
+      }),
+    }),
+  ],
 })
 export class AuthModule {}
