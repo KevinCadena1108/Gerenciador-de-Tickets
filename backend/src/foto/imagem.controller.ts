7933013347754d7214093dc80ac42a4d2874bb05
luca <lukzbe@hotmail.com>
2024-11-05 21:44:26 -0300

    ADD download user image

--- backend/src/foto/imagem.controller.ts
+++ backend/src/foto/imagem.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  UnauthorizedException,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ImagemService } from './imagem.service';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 @Controller('imagem')
 export class ImagemController {
-  constructor(private readonly imagemService: ImagemService) {}
+  constructor(
+    private readonly imagemService: ImagemService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Post('upload/:cpf')
   @UseInterceptors(
@@ -24,5 +37,13 @@ export class ImagemController {
   )
   async uploadFile(@Param('cpf') cpf: string, @UploadedFile() file: Express.Multer.File) {
     return await this.imagemService.saveImage(cpf, file);
+  }
+
+  @Get('download/:cpf')
+  async download(@Param('cpf') cpf: string, @Query('api-key') apiKey: string) {
+    if (apiKey !== this.configService.get('DOWNLOAD_API_KEY'))
+      throw new UnauthorizedException('API Key inválida!');
+
+    return await this.imagemService.download(cpf);
   }
 }
--- backend/src/foto/imagem.service.ts
+++ backend/src/foto/imagem.service.ts
@@ -1,14 +1,29 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, StreamableFile } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { ClienteRepository } from 'src/repository/cliente.repository';
+import { createReadStream } from 'fs';
 
 @Injectable()
 export class ImagemService {
-  constructor(private readonly clienteRespository: ClienteRepository) {}
+  constructor(
+    private readonly clienteRespository: ClienteRepository,
+    private readonly configService: ConfigService,
+  ) {}
 
   async saveImage(cpf: string, file: Express.Multer.File) {
     const cliente = await this.clienteRespository.findOne({ cpf: cpf });
     if (!cliente) throw new NotFoundException('O cliente não foi encontrado!');
     await this.clienteRespository.update({ imagem: file.path }, { cpf: cpf });
     return { message: 'Image uploaded successfully', filename: file.filename };
+  }
+
+  async download(cpf: string) {
+    const cliente = await this.clienteRespository.findOne({ cpf: cpf });
+    if (!cliente) throw new NotFoundException('O cliente não foi encontrado!');
+    if (!cliente.imagem) throw new NotFoundException('O cliente não possui imagem!');
+
+    // TODO
+    const file = createReadStream(cliente.imagem);
+    return new StreamableFile(file);
   }
 }
